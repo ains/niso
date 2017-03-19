e7153c95518cd78fced15e0a84616797a5df7607
Ainsley Escorce-Jones <me@ains.co>
2017-03-18 22:08:34 -0700

    Fix comment

--- storage.go
+++ storage.go
@@ -22,14 +22,13 @@ type Storage interface {
 	// RemoveAuthorize revokes or deletes the authorization code.
 	DeleteAuthorizeData(ctx context.Context, code string) error
 
-	// SaveAccess writes AccessData.
-	// If RefreshToken is not blank, it must save in a way that can be loaded using LoadRefresh.
+	// SaveAccess writes AccessData to storage.
 	SaveAccessData(ctx context.Context, data *AccessData) error
 
 	// GetRefreshTokenData retrieves refresh token data from the token string.
 	GetRefreshTokenData(ctx context.Context, token string) (*RefreshTokenData, error)
 
-	// SaveRefreshTokenData
+	// SaveRefreshTokenData saves refresh token data so it can be retrieved with GetRefreshTokenData
 	SaveRefreshTokenData(ctx context.Context, data *RefreshTokenData) error
 
 	// DeleteRefreshTokenData revokes or deletes a RefreshToken.
